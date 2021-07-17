8ba4c485046b3e8386d3ec787543cdfada2f210a
Danny <q123wer2002@gmail.com>
2021-07-17 22:11:01 +0800

    123

---
+++ leadtec_new/WebData/JsFile/Internal/Common/Components/CVue_LeftMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+  name: 'LeftMenu',
+  components: {},
+  props: {
+    title: String,
+    menuItem: Array,
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  created() {},
+  mounted() {
+  },
+  computed: {
+    ...mapState(['languageState', 'menuAry'])
+  },
+  watch: {},
+};
+</script>
+
+<style scoped>
+  
+</style>
